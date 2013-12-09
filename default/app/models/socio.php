14c4a768fc5b43800aa94d7f920e680d3a118680
José Fantasia <jose.fantasia@geneos.com.ar>
2013-12-09 12:37:10 -0300

    modificaciones de entidades para el banco de tiempo

--- default/app/models/socio.php
+++ default/app/models/socio.php
@@ -5,6 +5,7 @@ class Socio extends ActiveRecord
      * Retorna los items para ser paginados
      *
      */
+    
    public function getItems($page, $ppage=20)
    {
        return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
--- default/app/models/tipodocumento.php
+++ default/app/models/tipodocumento.php
@@ -9,4 +9,12 @@ class Tipodocumento extends ActiveRecord
    {
        return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
    }
+   
+   /**
+     * Método para obtener el nombre del tipo de documento
+     */
+    public static function getName($id) {
+        $tipo = Load::model('tipodocumento')->find_first($id);
+        return $tipo->nombre;
+    }
 }
